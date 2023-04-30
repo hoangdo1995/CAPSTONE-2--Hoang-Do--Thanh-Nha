74959466403126d76398c7e398824ea21c9dba82
dohoang790@gmail.com <dohoang790@gmail.com>
2023-04-30 18:20:02 +0700

    10

--- javascripts/detailPage.js
+++ javascripts/detailPage.js
@@ -143,7 +143,9 @@ window.onload = () =>{
         console.log('cardList',cardList);
         setCardStorange(cardList);
         document.querySelector('.shopping span').innerHTML = `( ${cardList.length})`;
-        window.location.href = '../pages/ShoppingCart.html';
+        // window.location.href = '../pages/ShoppingCart.html';
+        document.querySelector('.shopping').click();
+
         
     }
 }
